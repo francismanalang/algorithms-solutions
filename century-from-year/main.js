0bcaf3654660d1011536643db5b7b1c5acd70eca
Francis Manalang <francismanalang7@gmail.com>
2022-10-31 06:04:46 +0000

    Code Signal organized

--- century-from-year/main.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-function solution(year) {
-  return Math.ceil(year / 100);
-}
--- check-palindrome/main.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-function solution(inputString) {
-  var reverse = inputString.split('').reverse().join('');
-  return inputString === reverse;
-}
