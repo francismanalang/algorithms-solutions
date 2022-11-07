7c4b289de5d66287268ea05403fd5fbec342f594
Francis Manalang <francismanalang7@gmail.com>
2022-11-07 06:57:50 +0000

    getting the min of the sequence

---
+++ code-signal/almost-increasing-sequence/main.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+function solution(sequence) {
+  const min = Math.min(...sequence);
+  // eslint-disable-next-line no-empty
+  for (let i = min; i < sequence.length; i++) {
+
+  }
+}
