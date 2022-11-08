8e38e0c360edae30b69f2557bb7307029f0316bb
Francis Manalang <francismanalang7@gmail.com>
2022-11-08 07:01:48 +0000

    Almost increasing sequence completed

--- code-signal/almost-increasing-sequence/main.js
+++ code-signal/almost-increasing-sequence/main.js
@@ -1,8 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 function solution(sequence) {
-  const min = Math.min(...sequence);
-  // eslint-disable-next-line no-empty
-  for (let i = min; i < sequence.length; i++) {
+  let counter = 0;
+  const s = sequence;
+  for (let i = 1; i < s.length; i++) {
+    if (s[i - 1] >= s[i]) {
+      counter++;
+      if (counter > 1) return false;
 
+      if (s[i - 2] >= s[i] && s[i - 1] >= s[i + 1]) return false;
+    }
   }
+  return true;
 }
