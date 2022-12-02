c78a2e9676d6c76c2b996f4433a29f6cadf05c9d
Francis Manalang <francismanalang7@gmail.com>
2022-12-02 07:57:19 +0000

    common character count alogorithm solved

--- code-signal/common-character-count/main.js
+++ code-signal/common-character-count/main.js
@@ -1,7 +1,8 @@
 function solution(s1, s2) {
   const freqCounter1 = {};
   const freqCounter2 = {};
-  // eslint-disable-next-line no-unused-vars
+  const allLetters = s1 + s2;
+  const allcharacters = [];
   var output = 0;
   for (var i = 0; i < s1.length; i++) {
     const letter = s1[i];
@@ -12,16 +13,25 @@ function solution(s1, s2) {
     }
   }
   for (var j = 0; j < s2.length; j++) {
-    const letter = s1[j];
+    const letter = s2[j];
     if (freqCounter2[letter]) {
       freqCounter2[letter] += 1;
     } else {
       freqCounter2[letter] = 1;
     }
   }
-  // const freq1Obect = Object.entries(freqCounter1);
-  // const freq2Obect = Object.entries(freqCounter2);
-  // console.log(freq1Obect, freq2Obect);
+  for (var k = 0; k < allLetters.length; k++) {
+    if (!allcharacters.includes(allLetters[k])) {
+      allcharacters.push(allLetters[k]);
+    }
+  }
+  for (var x = 0; x < allcharacters.length; x++) {
+    const character = allcharacters[x];
+    if (freqCounter1[character] && freqCounter2[character]) {
+      output += Math.min(freqCounter1[character], freqCounter2[character]);
+    }
+  }
+  return output;
 }
 
-solution('aabcc', 'adcaa');
+solution('aaabcc', 'adccaa');
