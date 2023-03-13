5a18cb550e5bbd6a6ec89698a7b5fb2e828bb450
Francis Manalang <francismanalang7@gmail.com>
2023-03-13 00:15:37 +0000

    C# practice

---
+++ cs-tutorial/declaring-variables/main.cs
@@ -0,0 +1,34 @@
+// delcaring Variables / Constants
+
+int number;
+// Case sensitive
+int Number = 1;
+
+const float PI = 3.14f;
+
+// identifiers cannot start w a number
+// Cannot start with a number
+// Use meaningful name --> fn -> firstName
+
+// For local variables: Camel Case
+int number;
+
+// For constants: Pascal Case
+const int MaxZoom = 5;
+
+/* Primitive Types
+byte -> 0 to 255
+short -> -32,768 to 32,767
+int -> -2.1B to 2.1B
+long -> ...
+float -> -3.4 x 10^38 to 3.4 x 10^38
+double -> ...  Default datatype for numbers
+decimal
+char -> unicode characters
+bool -> True/False
+*/
+
+// Since double is the default datatype, ypu need to explicitly write m or f if you want float or decimal
+
+float number = 1.2f;
+decimal number = 1.2m;
